92abbef38cd3f090da705faebc186e6ce9a8f6de
zooogi <zooogi13@gmail.com>
2025-08-16 16:48:29 +0100

    added images

--- scr/model_checking_exp.R
+++ scr/model_checking_exp.R
@@ -38,7 +38,7 @@ ggplot(filter(dat_all, delta == 1),
        title = "ECDF of event times (conditional on being uncensored)") +
   theme_bw()
 #save
-ggsave("images/ppc_event_ecdf_exp.png", width = 6, height = 4)
+
 
 #ECDF（删失子样本）
 ggplot(filter(dat_all, delta == 0),
@@ -49,7 +49,6 @@ ggplot(filter(dat_all, delta == 0),
        title = "ECDF of censored durations (conditional on censoring)") +
   theme_bw()
 #save
-ggsave("images/ppc_censored_ecdf_exp.png", width = 6, height = 4)
 
 
 ggplot(fake_postpred_modelcheck_exp, aes(x = time)) +
@@ -59,10 +58,6 @@ ggplot(fake_postpred_modelcheck_exp, aes(x = time)) +
     x = "Months",
     y = "Count") +
   theme_minimal(base_size = 20)
-ggsave("images/fake_duration_hist_a200.png", width = 6, height = 4)
-
-
-
 
 
 ######################################
@@ -101,65 +96,93 @@ dat_a1000 <- bind_rows(real, fake_a1000)
 #----------plot----------------
 #a=-30-------------------
 # ECDF（事件子样本）
-ggplot(filter(dat_a30, delta == 1),
+base <- 15
+p31<-ggplot(filter(dat_a30, delta == 1),
        aes(x = Y, colour = source)) +
   stat_ecdf(size = 1) +
   labs(x = "Y | delta = 1 (event times)",
        y = "ECDF",
        colour = "source"
   ) +
-  theme_bw(base_size = 19)
+  theme_bw(base_size = base)
 #save
 ggsave("images/ppc_event_ecdf_A30.png", width = 6, height = 4, dpi = 300)
 
 #ECDF（删失子样本）
-ggplot(filter(dat_a30, delta == 0),
+p32<-ggplot(filter(dat_a30, delta == 0),
        aes(x = Y, colour = source)) +
   stat_ecdf(size = 1) +
   labs(x="Y | delta = 0 (censored durations)",
        y = "ECDF"
        ) +
-  theme_bw(base_size = 19)
+  theme_bw(base_size = base)
 #save
 ggsave("images/ppc_censored_ecdf_A30.png", width = 6, height = 4, dpi = 300)
 
 
-ggplot(fake_ppc_a30_exp, aes(x = time)) +
+p33<-ggplot(fake_ppc_a30_exp, aes(x = time)) +
   geom_histogram(binwidth = 3, fill = "#D55E00", alpha = 0.7) +
   facet_wrap(~ event, labeller = labeller(event = c("0" = "Censored", "1" = "Event Occurred"))) +
   labs(
        x = "Months",
        y = "Count") +
-  theme_minimal(base_size = 20)
-ggsave("images/fake_duration_hist_a30.png", width = 6, height = 4)
+  theme_minimal(base_size = 21)
+p33
+ggsave("images/fake_duration_hist_a30.pdf",plot = p33,device = "pdf", 
+       width = 6, height = 4)
+
+combo <- (p31 + p32 ) +
+  plot_layout(guides = "collect", widths = c(1,1)) &
+  theme(legend.position = "right")
+combo
+ggsave("images/ppc_two_a30.pdf",
+       plot   = combo,
+       width  = 7,   # 目标在论文中的实际宽度（英寸）
+       height = 2.8,   # 合理高度，别太矮
+       device = "pdf") 
+
+
 
 
 #------------------------a=-1000-----------------
 # ECDF（事件子样本）
-ggplot(filter(dat_a1000, delta == 1),
+p1k1<-ggplot(filter(dat_a1000, delta == 1),
        aes(x = Y, colour = source)) +
   stat_ecdf(size = 1) +
   labs(x = "Y | delta = 1 (event times)",
        y = "ECDF") +
-  theme_bw(base_size = 19)
+  theme_bw(base_size = base)
 #save
 ggsave("images/ppc_event_ecdf_A1000.png", width = 6, height = 4, dpi = 300)
 
 
 #ECDF（删失子样本）
-ggplot(filter(dat_a1000, delta == 0),
+p1k2<-ggplot(filter(dat_a1000, delta == 0),
        aes(x = Y, colour = source)) +
   stat_ecdf(size = 1) +
   labs(x="Y | delta = 0 (censored durations)",
        y = "ECDF") +
-  theme_bw(base_size = 19)
+  theme_bw(base_size = base)
 #save
 ggsave("images/ppc_censored_ecdf_A1000.png", width = 6, height = 4, dpi = 300)
 
-ggplot(fake_ppc_a1000_exp, aes(x = time)) +
+p1k3<-ggplot(fake_ppc_a1000_exp, aes(x = time)) +
   geom_histogram(binwidth = 3, fill = "#D55E00", alpha = 0.7) +
   facet_wrap(~ event, labeller = labeller(event = c("0" = "Censored", "1" = "Event Occurred"))) +
   labs(x = "Months",
        y = "Count") +
-  theme_minimal(base_size = 20)
-ggsave("images/fake_duration_hist_a1000.png", width = 6, height = 4)
+  theme_minimal(base_size = 21)
+p1k3
+ggsave("images/fake_duration_hist_a1000.pdf",plot = p1k3,device = "pdf", 
+       width = 6, height = 4)
+
+
+comb <- (p1k1 + p1k2 ) +
+  plot_layout(guides = "collect", widths = c(1,1)) &
+  theme(legend.position = "right")
+comb
+ggsave("images/ppc_two_a1000.pdf",
+       plot   = comb,
+       width  = 7,   # 目标在论文中的实际宽度（英寸）
+       height = 2.8,   # 合理高度，别太矮
+       device = "pdf") 
